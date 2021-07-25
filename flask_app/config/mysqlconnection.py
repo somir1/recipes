ca4ea937274df1780fb881e26699bdff6d6f2c63
somir1 <leonsomir@gmail.com>
2021-07-25 02:04:47 -0500

    updated sql password generate requirement.txt file

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -10,7 +10,7 @@ class MySQLConnection:
 # change the user and password as needed
         connection = pymysql.connect(host='localhost',
         user='root',
-        password='root',
+        password='shossain4',
         db=db,
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor,
